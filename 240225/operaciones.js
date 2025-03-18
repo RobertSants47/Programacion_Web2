d13627babcfd99f3ea4b92eefe7ff7eb382b662e
RobertSants47 <robertsants43@gmail.com>
2025-03-18 10:42:18 -0600

    Modificación en el archivo operaciones

--- 240225/operaciones.js
+++ 240225/operaciones.js
@@ -15,16 +15,33 @@ function division(numero1,numero2){
 }
 
 let opcion = parseInt(prompt(`
-    Operacion: 
-    1. Ingresa 1 para hacer una suma`));
+    Elige la operación que quieres realizar: 
+    1. Suma
+    2. Resta
+    3. Multiplicación
+    4. Division`));
+
+let numero1 = prompt("Ingresa el número1: ");
+let numero2 = prompt("Ingresa el número2: ");
+
 switch (opcion){
     case 1:
         console.log(`La suma es: ${suma(numero1,numero2)}`);
     break;
     case 2:
-        console.log(`La resta es: ${suma(numero1,numero2)}`);        
+        console.log(`La resta es: ${resta(numero1,numero2)}`);
+    break;
+    case 3:
+        console.log(`La multiplicación es: ${multiplicacion(numero1,numero2)}`);
+    break;
+    case 4:
+        console.log(`La división es: ${division(numero1,numero2)}`);
+    break;
+    default:
+        console.log("¡ERROR! No has seleccionado ningua opción");        
     
     }
+
 /*
 let n1 = prompt("Ingresa un número1: ");
 let n2 = prompt("Ingresa un número2: ");

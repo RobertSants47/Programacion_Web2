3677f967a54b6252ec53c29f8d97740e561fb1a2
RobertSants47 <robertsants43@gmail.com>
2025-04-02 12:51:52 -0600

    Clase del 02042025 getElemetByID

---
+++ 020425/app.js
@@ -0,0 +1,9 @@
+/*
+console.log(document);
+console.log(document.title);
+console.log(document.body);
+*/
+
+console.log( document.getElementById("tituloWeb"));
+// contenido interno 
+console.log(document.getElementById("tituloWeb").textContent);
---
+++ 020425/app1.js
@@ -0,0 +1,8 @@
+// console.log(document.querySelector("h1"));
+// console.log(document.querySelector(".text-primary2"));
+// console.log(document.querySelector("#tituloWeb3"));
+
+console.log(document.querySelector(".texto"));
+console.log(document.querySelectorAll(".texto"));
+
+console.log(document.querySelector(".contenido .texto"));
